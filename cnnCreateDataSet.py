ffa264332e735b758af83eca151d51195e5f611f
PoojaK97 <thisispooja97@gmail.com>
2020-06-14 13:48:13 +0530

    create dataset

--- cnnCreateDataSet.py
+++ cnnCreateDataSet.py
@@ -1,47 +1,38 @@
-# -*- coding: utf-8 -*-
-
-
 import cv2
-import cnnPredict
 import cnnFilters
-import cnnTrain
-import cnnModel
-import cnnCreateDataSet
 import time
+from gtts import gTTS
 import os
 
 x0 = 400
 y0 = 200
 height = 200
 width = 200
-isBgModeOn = 0
-isAdaptiveThresholdMode = True
-roi = None
-isPredictionMode = False
-model= None
-menu = "\n c-Change Filter\n p-Predict Sign\n n-Save ROI in data set \n q-Close ROI Window \n w-Move ROI Upside\n s-Move ROI Downside\n a-Move ROI Rightside\n d-Move ROI Leftside\n ESC-exit\n"
-
-#remove background
 
 def Main():
-    global isAdaptiveThresholdMode, isBgModeOn,x0,y0,roi,isPredictionMode,model,menu
+    global x0, y0
     isQuit=0
+    tts = gTTS(text="Change the gesture", lang='en')
+    tts.save("dataset.mp3")
     cap = cv2.VideoCapture(0)
     ret = cap.set(3,640)
     ret = cap.set(4,480)
     i=0
     j=0
     #sign array should be in order 
-    signnamearray = ["Aboard", "Baby", "Bowl","Friend"," House" ,"IorMe","Money","Opposite","Prisoner","You"]
+    signnamearray = ["Aboard", "All_Gone", "Baby", "Beside", "Book", "Bowl", "Bridge", "Camp", "Cartridge", 
+    "Eight", "Five", "Fond", "Four", "Friend", "Glove", "Hang", "High", "House", "How_Many","IorMe", "Man", "Marry",
+    "Meat", "Medal", "Mid-day", "Middle", "Money", "Moon", "Mother", "Nine", "One","Opposite","Prisoner", "Ring", "Rose", "See",
+    "Seven", "Short", "Six", "Superior", "Ten", "Thick", "Thin", "Three", "Tobacco", "Two", "Up", "Watch", "Write", "You"]
     while(True):
         ret, frame = cap.read()
         #invert frame
-        frame = cv2.flip(frame, 3)
+        frame = cv2.flip(frame, 3) 
 
         roi1 = cnnFilters.adaptiveThresholdMode(frame, x0, y0, width, height)
         roi2 = cnnFilters.siftMode(frame, x0, y0, width, height)
         roi3 = cnnFilters.noFilterMode(frame, x0, y0, width, height)
-        cv2.imshow('Sign Language Detactor',frame)
+        cv2.imshow('Sign Language Dataset Capture',frame)
 
         if not isQuit:
             cv2.imshow('ROI1', roi1)
@@ -58,25 +49,32 @@ def Main():
             path1="./AdaptiveThresholdModeDataSet/"
             path2="./SiftModeDataSet/"
             path3="./NoFilterModeDataSet/"
+            '''
+            timestamp is not used in the final dataset naming
             ts = int(time.time())
             name = signname + str(ts)
             print ("creating image...")
             cv2.imwrite(path1+name + str(j) + "1.png", roi1)
             cv2.imwrite(path2+name + str(j) + "2.png", roi2)
             cv2.imwrite(path3+name + str(j) + "3.png", roi3)
+            '''
+            print ("creating image...")
+            cv2.imwrite(path1+str(signname) + str(j) + ".png", roi1)
+            cv2.imwrite(path2+str(signname) + str(j) + ".png", roi2)
+            cv2.imwrite(path3+str(signname) + str(j) + ".png", roi3)
             print ("created image: "+str(signname)+ " " + str(j) + " for word " + str(signname))
             j=j+1
-            #number of images per sign
-            if(j==10):
-              i=i+1
-              j=0
-              os.system('spd-say "Change The Gesture"')
-              #Total number of signs
-              if(i==10):
-                  break
+            #number of images per sign, 100 for our dataset
+            if(j==100):
+                i=i+1
+                j=0
+                #Total number of signs
+                if(i==50):
+                    break
+                os.system("start dataset.mp3")
             time.sleep(0.04 )
         elif key == ord('q'):
-             isQuit = not isQuit
+            isQuit = not isQuit
         elif key == ord('w'):
             y0 = y0 - 5
         elif key == ord('s'):
@@ -89,8 +87,6 @@ def Main():
             break
     cap.release()
     cv2.destroyAllWindows()
-
-
 
 if __name__ == "__main__":
     Main()
